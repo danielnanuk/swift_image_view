17243783332376fe47af2b515cb6081e6784f396
daniel <danielnanuk@gmail.com>
2015-07-29 08:28:42 +0800

    ImageView

--- ImageViewTest/ViewController.swift
+++ ImageViewTest/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var backgroundImage : UIImageView?
+    @IBOutlet var myLabel : UILabel?
+    
+    var imageList : [String] = ["DDOS_1.jpg","DDOS_2.jpg"];
+    
+    var nameList : [String] = ["A ", "B "];
+    
+    let maxImages = 1;
+    var imageIndex :NSInteger = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        backgroundImage!.image = UIImage(named: imageList[imageIndex])
+        myLabel!.text = nameList[imageIndex]
+        
+        
     }
 
+    @IBAction func shareImage(){
+        let firstActivityItem = "Check out my great picture"
+        let pictureActivityItem : UIImage = UIImage(named: imageList[imageIndex])!
+        
+        let aV : UIActivityViewController = UIActivityViewController(activityItems: [firstActivityItem,pictureActivityItem], applicationActivities: nil);
+        
+        presentViewController(aV, animated: true, completion: nil);
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextImage(){
+        imageIndex++
+        if imageIndex > maxImages {
+            imageIndex = 0
+        }
+        backgroundImage!.image = UIImage(named: imageList[imageIndex])
+        myLabel!.text = nameList[imageIndex]
+    }
 
 }
 
